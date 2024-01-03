3d0f92c2b2339cfad8ff3cb1541075c58b3517f5
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-02 21:47:14 -0500

    Update index.py

--- index.py
+++ index.py
@@ -5,3 +5,7 @@ class Student:
         self.student_id = student_id
         self.name = name
         self.grade = grade
+class SchoolManager:
+    def __init__(self, students_filename="students.pkl"):
+        self.students_filename = students_filename
+        self.students = self.load_students()
