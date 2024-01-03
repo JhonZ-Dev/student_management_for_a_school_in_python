73948822a65f73f2cec6e920e0b41f1a75ae9d07
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-02 21:47:02 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,2 +1,7 @@
 import os
 import pickle
+class Student:
+    def __init__(self, student_id, name, grade={}):
+        self.student_id = student_id
+        self.name = name
+        self.grade = grade
